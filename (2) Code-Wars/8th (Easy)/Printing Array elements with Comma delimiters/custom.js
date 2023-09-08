350772e9ff5b8acbab6b93fba812362aa16ee4a8
Marawan <loew.krs@yahoo.com>
2023-09-08 14:19:17 +0300

    9/8/2023

---
+++ (2) Code-Wars/8th (Easy)/Printing Array elements with Comma delimiters/custom.js
@@ -0,0 +1,20 @@
+function printArray(array){
+  return array.join(",");
+}
+
+
+
+console.log(printArray([2, 4.2, 5.1, 2.2]));
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ (2) Code-Wars/8th (Easy)/Welcome to the City/custom.js
@@ -0,0 +1,21 @@
+function sayHello( name, city, state ) {
+  return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
+}
+
+
+
+console.log(sayHello(['John', 'Smith'], "Phoenix", "Arizona"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
