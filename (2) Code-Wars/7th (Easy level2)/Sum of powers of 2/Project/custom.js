3582059a769d44915df88d2d5e7feff9dfa1e9ce
Marawan Saqr <loew.krs@yahoo.com>
2024-11-17 10:31:51 +0200

    17/11/2024

---
+++ (2) Code-Wars/7th (Easy level2)/Sum of powers of 2/Project/custom.js
@@ -0,0 +1,18 @@
+const powers = n => {
+
+  let result = [];
+  let i = 0;
+
+  while (n > 0) {
+    if (n % 2 === 1) {
+      result.push(Math.pow(2, i));
+    }
+    n = Math.floor(n / 2);
+    i++;
+  }
+
+  return result;
+};
+
+
+console.log(powers(6));
