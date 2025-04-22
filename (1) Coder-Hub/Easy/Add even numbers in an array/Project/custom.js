2941869e2634fd3ce254906bd914378f5fe32b29
Marawan Saqr <loew.krs@yahoo.com>
2025-04-22 23:45:01 +0200

    22/04/202

--- (1) Coder-Hub/Easy/Add even numbers in an array/Project/custom.js
+++ (1) Coder-Hub/Easy/Add even numbers in an array/Project/custom.js
@@ -1,14 +1,11 @@
 function sum_even(arr) {
 
-  let result = 0;
+  let filtered = arr.filter((element)=> {
+    return element % 2 === 0;
+  })
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      result += arr[i];
-    }
-  }
+  return filtered;
 
-  return result;
 }
 
 console.log(sum_even([11, 0, 5, 22]));
