317c0eea1ff6e9b370203d2b5c094ba8ac820dfc
Marawan Saqr <loew.krs@yahoo.com>
2025-04-23 15:44:34 +0200

    23/04/2025

--- (1) Coder-Hub/Easy/Add odd numbers into an array/Project/custom.js
+++ (1) Coder-Hub/Easy/Add odd numbers into an array/Project/custom.js
@@ -1,11 +1,8 @@
 function sumOdd(arr) {
 
   let result = 0;
-
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      continue;
-    } else {
+    if (arr[i] % 2 !== 0) {
       result += arr[i];
     }
   }
