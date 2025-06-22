e61608453e2340a33e5d068681bf5905bedd314d
Marawan Saqr <loew.krs@yahoo.com>
2025-06-22 18:37:55 +0300

    22/06/2025

--- (2) Code-Wars/8th (Easy)/A Needle in the Haystack/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/A Needle in the Haystack/Project/custom.js
@@ -2,7 +2,7 @@ function findNeedle(haystack) {
 
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === "needle") {
-      return `found the needle at position ${haystack.indexOf("needle")}`;
+      return `found the needle at position ${i}`;
     }
   }
 
