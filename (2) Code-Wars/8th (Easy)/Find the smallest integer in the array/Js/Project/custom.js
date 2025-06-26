d2f3042d213893fb851357bede618179e3ebded2
Marawan Saqr <loew.krs@yahoo.com>
2025-06-26 18:48:21 +0300

    26/06/2025

--- (2) Code-Wars/8th (Easy)/Find the smallest integer in the array/Js/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/Find the smallest integer in the array/Js/Project/custom.js
@@ -1,14 +1,7 @@
 function findSmallestInt(arr) {
 
-  let smallNumber = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (smallNumber > arr[i]) {
-      smallNumber = arr[i];
-    }
-  }
-
-  return smallNumber;
+  let smallest = Math.min(...arr);
+  return smallest;
 
 }
 
