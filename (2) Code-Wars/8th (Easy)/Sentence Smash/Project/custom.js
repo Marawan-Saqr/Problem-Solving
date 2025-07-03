41db0a47262ea446793586a3f3ac24a5bae2e529
Marawan Saqr <loew.krs@yahoo.com>
2025-07-03 21:13:35 +0300

    03/07/2025

--- (2) Code-Wars/8th (Easy)/Sentence Smash/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/Sentence Smash/Project/custom.js
@@ -2,20 +2,6 @@ function smash(words) {
   return words.join(" ");
 };
 
-console.log(smash(['hello', 'world', 'this', 'is', 'great']));
-
-//////////////////////////////////////////////////////////////////////////////////
-
-// Another Solution
-
-function smash(words) {
-  let result = words.reduce(function(acc, current) {
-    return `${acc} ${current}`;
-  }, "");
 
 
-  return result.trim();
-
-}
-
-console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+console.log(smash(["hello", "amazing", "world"]));
