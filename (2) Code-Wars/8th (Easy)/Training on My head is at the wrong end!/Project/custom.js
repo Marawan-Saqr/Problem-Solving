e067a8211e9795d3f1afd0ba382bbde6e87bb683
Marawan Saqr <loew.krs@yahoo.com>
2025-07-23 22:32:12 +0300

    23/07/2025

--- (2) Code-Wars/8th (Easy)/Training on My head is at the wrong end!/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/Training on My head is at the wrong end!/Project/custom.js
@@ -1,6 +1,23 @@
 function fixTheMeerkat(arr) {
-  return arr.reverse();
+
+  let temp = arr[0]; // tail
+  let one = arr[2];  // head
+  let two = temp;    // tail
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      result.push(one);
+    } else if (i === 2) {
+      result.push(two);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+
 }
 
-console.log(fixTheMeerkat(["tail", "body", "head"]));
 
+console.log(fixTheMeerkat(["tail", "body", "head"]));
