72c675ab9454482713b9b2cab7ef0b4b675372c0
Marawan Saqr <loew.krs@yahoo.com>
2025-08-30 21:00:05 +0300

    30/08/2025

--- (2) Code-Wars/8th (Easy)/Correct the mistakes of the character recognition software/Js/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/Correct the mistakes of the character recognition software/Js/Project/custom.js
@@ -1,38 +1,22 @@
 function correct(string) {
 
-  let result = string.split("").map(function(e) {
-    if (e === "0") {
-      return "O";
-    } else if (e === "5") {
-      return "S";
-    } else if (e === "1") {
-      return "I";
-    } else {
-      return e;
-    }
-  })
-
-  return result.join("");
-
-}
-
-console.log(correct("L0ND0N"));
-
-///////////////////////////Another Method//////////////////////////////////////
-function correct(string){
   let updatedString = "";
   for (let i = 0; i < string.length; i++) {
-    if (string[i].includes("0")) {
+    if (string[i] === "0") {
       updatedString += string[i].replace("0", "O");
-    } else if (string[i].includes("5")) {
+    } else if (string[i] === "5") {
       updatedString += string[i].replace("5", "S");
-    } else if (string[i].includes("1")) {
+    } else if (string[i] === "1") {
       updatedString += string[i].replace("1", "I");
     } else {
       updatedString += string[i];
     }
   }
+
   return updatedString;
+
 }
+
+
 
 console.log(correct("L0ND0N"));
