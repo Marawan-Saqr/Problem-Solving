87bdf7fdcb307e270ac45c90e037cd1a0e443c6b
Marawan <loew.krs@yahoo.com>
2026-05-11 20:06:33 +0300

    Reviewed 5 problems 5/11/2026

--- (2) Code-Wars/8th (Easy)/Multiply the number/Project/custom.js
+++ (2) Code-Wars/8th (Easy)/Multiply the number/Project/custom.js
@@ -1,6 +1,5 @@
-function multiply(number){
-  let numberDigits = Math.abs(number).toString().length;
-  return number * Math.pow(5, numberDigits);
+function multiply(number) {
+  return number * Math.pow(5, number > 0 ? number.toString().length : (number * -1).toString().length);
 }
 
 console.log(multiply(-3));
