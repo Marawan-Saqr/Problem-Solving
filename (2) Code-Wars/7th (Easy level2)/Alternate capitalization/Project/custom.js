ed296206aeb9f27455dfe6699079a3b0fd41ec2b
Marawan <loew.krs@yahoo.com>
2026-07-16 19:11:32 +0300

    Reviewed 1 Problem

--- (2) Code-Wars/7th (Easy level2)/Alternate capitalization/Project/custom.js
+++ (2) Code-Wars/7th (Easy level2)/Alternate capitalization/Project/custom.js
@@ -1,34 +1,24 @@
 function capitalize(s){
 
-  let splited = s.split("");  // ['a', 'b', 'c', 'd', 'e', 'f']
-
-  // For First Result
-  let firstResult = [];
-  for (let i = 0; i < splited.length; i++) {
+  let evenResult = [];
+  for (let i = 0; i < s.length; i++) {
     if (i % 2 === 0) {
-      firstResult.push(splited[i].toUpperCase());
+      evenResult.push(s[i].toUpperCase());
     } else {
-      firstResult.push(splited[i].toLowerCase());
+      evenResult.push(s[i]);
     }
   }
 
-  // For Second Result
-  let secondResult = [];
-  for (let j = 0; j < splited.length; j++) {
-    if (j % 2 !== 0) {
-      secondResult.push(splited[j].toUpperCase());
+  let oddResult = [];
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 !== 0) {
+      oddResult.push(s[i].toUpperCase());
     } else {
-      secondResult.push(splited[j].toLowerCase());
+      oddResult.push(s[i]);
     }
   }
 
-
-  let finalResult = [];
-
-  finalResult.push(firstResult.join(""));
-  finalResult.push(secondResult.join(""));
-
-  return finalResult;
+  return [evenResult.join(""), oddResult.join("")];
 
 }
 
